e374d7ae348a51d6b57e40efdd60a39c9349157b
ferid aqa <afe681a2@gmail.com>
2023-10-17 00:19:25 +0400

    a

---
+++ scrtpt.js
@@ -0,0 +1,77 @@
+///1 
+/*
+#############################################
+callback i harda istifadə etməliyəm tam olara bilmədim
+#############################################
+*/
+//herbir elementi 2 bolub cemliyerem 
+// ve ya cemliyib onun sayina bolerem
+
+// function ededi_orta(...arr) {
+//     const result = [...arr];
+//     let sum = 0;
+//     for (let i = 0; i < result.length; i++) {
+//         sum += result[i];
+//     }
+
+//      return sum / result.length;
+// }
+
+
+// console.log(ededi_orta(3,5,5,3,14));
+
+
+//2
+
+// function cem(...arr) {
+//     const result = [...arr];
+//     let sum = 0;
+//     for (let i = 0; i < result.length; i++) {
+//         if (result[i]%2===0) {
+//              sum+=result[i]
+//         }
+//     }
+//     return sum
+// }
+
+// console.log(cem(2,5,5,3,14));
+
+
+//3
+
+// function cem(...arr) {
+//     const result = [...arr];
+//     let sum = 0;
+//     for (let i = 0; i < result.length; i++) {
+//         if (result[i]%2!==0) {
+//              sum+=result[i]
+//         }
+//     }
+//     return sum
+// }
+
+// console.log(cem(2,5,5,3,14));
+
+
+
+//4
+
+// function ededi_orta(...arr) {
+//     const result = [...arr];
+//     let sumcut = 0;
+//     let sumtek = 0;
+//     for (let i = 0; i < result.length; i++) {
+
+//         if (result[i]%2===0) {
+//              sumcut+=result[i]
+//         }
+//         if (result[i]%2!==0) {
+//              sumtek+=result[i]
+//         }
+//     }
+//     return sumcut*sumtek
+// }
+
+// console.log(ededi_orta(2,5,3,8));
+
+
